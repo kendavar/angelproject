09dbc86d47d4c02812307cdeeafe0254be125afc
Karanam kendavar <kendacool@gmail.com>
2018-07-23 18:19:10 +0530

    Added index view

--- angelapp/views.py
+++ angelapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View,TemplateView
 
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'base.html'
+ 
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASE INJECTION!'
+        return context
+        
