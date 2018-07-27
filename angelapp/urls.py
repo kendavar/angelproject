e671e54a0bbc2726c7dec09c91f37eea12271ccb
kendavar <kendavar@u593016f3c1075c.ant.amazon.com>
2018-07-27 20:04:16 +0530

    changes done in files

--- angelapp/urls.py
+++ angelapp/urls.py
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-from django.config.urls import url
+from django.conf.urls import url
 from . import views
 
 app_name = "angelapp"
 
 urlpatterns = [
     url(r'^$',views.HomePage.as_view(),name='homepage')
-=======
-from django.conf.urls import url
-
-app_name = 'angelapp'
-
-urlpatterns = [
-    url(r'profile/$',views.profile,name="profile")
->>>>>>> da9c7dcfe043aa48db7a1b6c558ebe240e01e471
 ]
--- angelproject/settings.py
+++ angelproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'angelapp'
+    'freespirit'
 ]
 
 MIDDLEWARE = [
@@ -53,6 +53,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'angelproject.urls'
+
+LOGIN_REDIRECT_URL = '/'
 
 TEMPLATES = [
     {
@@ -102,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'freespirit.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
